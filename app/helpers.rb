f440b9464ca7979c2155bed4fb27f3a92026ff81
marutanm <marutanm@gmail.com>
2013-07-18 23:31:03 +0900

    Show progress bar

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,7 +1,18 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 FastTime::App.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def weekdays(year = nil, month = nil)
+    now = Time.now
+    year = now.year unless year
+    month = now.month unless month
+
+    first_day = Date.new year, month, 1
+    last_day = Date.new year, month, -1
+
+    days = (first_day..last_day).to_a
+    days.delete_if {|day| day.saturday?}
+    days.delete_if {|day| day.sunday?}
+    days.delete_if {|day| HolidayJp.holiday? day}
+    days.count
+  end
 end
