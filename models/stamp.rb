1e5bec6d19732746c0910b2cf093a35822788ef6
marutanm <marutanm@gmail.com>
2013-08-02 13:34:32 +0900

    Add fields to stamp model

--- models/stamp.rb
+++ models/stamp.rb
@@ -4,7 +4,20 @@ class Stamp
 
   embedded_in :user
 
+  field :start_time, type: Time
+  field :end_time,   type: Time
+
   def working_time
-    self.updated_at - self.created_at
+    self.end_time - self.start_time - 1.hour
+  end
+
+  def start_time
+    return super if super
+    self.created_at
+  end
+
+  def end_time
+    return super if super
+    self.updated_at
   end
 end
